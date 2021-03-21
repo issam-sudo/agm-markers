b4de5b4b5a45c00d6d6ce1e995ed7e167da06328
issam-sudo <issambenkaroum95@gmail.com>
2021-03-21 22:35:41 +0100

    [feat] new interface

--- src/app/models/marker.model.ts
+++ src/app/models/marker.model.ts
@@ -1,19 +1,12 @@
-export class marker {
+export interface marker {
     lat: number;
     lng: number;
     label: any;
     urlIcon: any;
     draggable: boolean;
   
-    constructor(Marker: { lat: number; lng: number; label: any; urlIcon: any; draggable: boolean; }) {
-        {
-          this.lat = Marker.lat;
-          this.lng = Marker.lng;
-          this.label = Marker.label;
-          this.urlIcon = Marker.urlIcon;
-          this.draggable = Marker.draggable;
-        }
+   
     }
-  }
+  
 
   
